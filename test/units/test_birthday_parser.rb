7c46a068eadd1287d33742371c6cfb054a3a85e5
Austin Erlandson <austin@erlandson.com>
2015-05-05 12:06:42 -0500

    Renamed some stuff and worked on tests:)
    Changes committed:
      modified:   cheers
      modified:   lib/cheer.rb
      modified:   test/integrations/test_cheers.rb
      modified:   test/units/test_birthday_parser.rb
      deleted:    test/units/test_cheer.rb
      new file:   test/units/test_cheer_class.rb

--- test/units/test_birthday_parser.rb
+++ test/units/test_birthday_parser.rb
@@ -6,5 +6,20 @@ class TestBirthdayParser < Minitest::Test
     assert_equal false, true
   end
 
+  def test_happy_path_with_birthday_in_future
+    fail
+  end
+
+  def test_happy_path_with_recent_birthday
+    fail
+  end
+
+  def test_happy_path_with_birthday_today
+    fail
+  end
+
+  def test_happy_path_with_birthday_in_past
+    fail
+  end
 
 end
