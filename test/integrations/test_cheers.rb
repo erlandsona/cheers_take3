c55d9de22bc3c8cd49e5e85f4cf848a6d7df76ad
Austin Erlandson <austin@erlandson.com>
2015-05-05 10:11:44 -0500

    Boils working

---
+++ test/integrations/test_cheers.rb
@@ -0,0 +1,10 @@
+require_relative '../test_helper'
+
+class TestCheers < Minitest::Test
+
+  def test_are_working
+    assert_equal false, true
+  end
+
+
+end
---
+++ test/units/test_birthday_parser.rb
@@ -0,0 +1,10 @@
+require_relative '../test_helper'
+
+class TestBirthdayParser < Minitest::Test
+
+  def test_are_working
+    assert_equal false, true
+  end
+
+
+end
