a393356c5c1088e35010c5da2780592905946a96
Austin Erlandson <austin@erlandson.com>
2015-05-05 22:03:50 -0500

    Cheer_Class is now passing first Unit tests:)

--- lib/cheer.rb
+++ lib/cheer.rb
@@ -1,7 +1,30 @@
 class Cheer
   def self.for_person(name)
-    "Give me an.. E!\n" +
-    "Give me a... D!\n" +
-    "Ed's just GRAND!"
+    raise ArgumentError unless valid_name?(name)
+    cheer = ""
+    cheerable_name = name.upcase.gsub(/[^A-Z]/, "")
+    cheerable_name.each_char do |char|
+      if "HALFNORSEMIX".include?(char)
+        article = "n.."
+      else
+        article = "..."
+      end
+      cheer << "Give me a#{article} #{char}!\n"
+    end
+    cheer << "#{name}'s just GRAND!"
   end
+
+
+  def self.valid_name?(name)
+    cheerable_name = name.upcase.gsub(/[^A-Z]/, "")
+    !cheerable_name.empty?
+  end
+
+
+
+
+
+
+
+
 end
