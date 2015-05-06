c09f9dc225f909c5005be646cad2fe0dcebb5670
Austin Erlandson <austin@erlandson.com>
2015-05-05 22:05:31 -0500

    Updated test_cheer_class.rb to spec

--- test/units/test_cheer_class.rb
+++ test/units/test_cheer_class.rb
@@ -10,23 +10,169 @@ class TestCheer < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_happy_path_name
+    actual = Cheer.for_person("Ed")
+    expected = "Give me an.. E!\n" +
+               "Give me a... D!\n" +
+               "Ed's just GRAND!"
+    assert_equal expected, actual
+  end
+
+
+
   def test_name_with_spaces
-    fail
+    actual = Cheer.for_person("Bo Bur")
+    expected = "Give me a... B!\n" +
+               "Give me an.. O!\n" +
+               "Give me a... B!\n" +
+               "Give me a... U!\n" +
+               "Give me an.. R!\n" +
+               "Bo Bur's just GRAND!"
+    assert_equal expected, actual
+  end
+
+  def test_name_with_spaces
+    actual = Cheer.for_person("Mary Jane")
+    expected = "Give me an.. M!\n" +
+               "Give me an.. A!\n" +
+               "Give me an.. R!\n" +
+               "Give me a... Y!\n" +
+               "Give me a... J!\n" +
+               "Give me an.. A!\n" +
+               "Give me an.. N!\n" +
+               "Give me an.. E!\n" +
+               "Mary Jane's just GRAND!"
+    assert_equal expected, actual
+  end
+
+
+
+
+
+
+  def test_name_with_hyphens
+    actual = Cheer.for_person("Bo-Bur")
+    expected = "Give me a... B!\n" +
+               "Give me an.. O!\n" +
+               "Give me a... B!\n" +
+               "Give me a... U!\n" +
+               "Give me an.. R!\n" +
+               "Bo-Bur's just GRAND!"
+    assert_equal expected, actual
   end
 
   def test_name_with_hyphens
-    fail
+    actual = Cheer.for_person("Mary-Jane")
+    expected = "Give me an.. M!\n" +
+               "Give me an.. A!\n" +
+               "Give me an.. R!\n" +
+               "Give me a... Y!\n" +
+               "Give me a... J!\n" +
+               "Give me an.. A!\n" +
+               "Give me an.. N!\n" +
+               "Give me an.. E!\n" +
+               "Mary-Jane's just GRAND!"
+    assert_equal expected, actual
   end
 
+
+
+
+
   def test_name_thats_blank
-    fail
+    actual = Cheer.for_person("")
+    expected = "I'm sorry, what was your name again?"
+    assert_equal expected, actual
   end
 
   def test_name_thats_all_whitespace
-    fail
+    assert_raises(ArgumentError) do
+      Cheer.for(" ")
+    end
   end
 
   def test_name_that_has_no_word_characters
-    fail
+    actual = Cheer.for_person(" ")
+    expected = "Give me an.. E!\n" +
+               "Give me a... D!\n" +
+               "Ed's just GRAND!"
+    assert_equal expected, actual
   end
+
+
+  def test_name_thats_blank
+    assert_raises(ArgumentError) do
+      Cheer.for_person("")
+    end
+  end
+
+  def test_name_thats_all_whitespace
+    assert_raises(ArgumentError) do
+      Cheer.for_person("  ")
+    end
+  end
+
+  def test_name_that_has_no_word_characters
+    assert_raises(ArgumentError) do
+      Cheer.for_person("*!?")
+    end
+  end
+
+  def test_birthday_instead_of_name
+    assert_raises(ArgumentError) do
+      Cheer.for_person("08/25")
+    end
+  end
+
+
+
+  def test_all_an_letters
+    actual = Cheer.for_person("AEFHILMNORSX")
+    expected = "Give me an.. A!\n" +
+               "Give me an.. E!\n" +
+               "Give me an.. F!\n" +
+               "Give me an.. H!\n" +
+               "Give me an.. I!\n" +
+               "Give me an.. L!\n" +
+               "Give me an.. M!\n" +
+               "Give me an.. N!\n" +
+               "Give me an.. O!\n" +
+               "Give me an.. R!\n" +
+               "Give me an.. S!\n" +
+               "Give me an.. X!\n" +
+               "AEFHILMNORSX's just GRAND!"
+    assert_equal expected, actual
+  end
+
+  def test_birthday_today
+    actual = Cheer.for_birthday("05/05")
+    expected = "Awesome! Your birthday is today! Happy Birthday!"
+    assert_equal actual, expected
+  end
+
+  def test_birthday_tomorrow
+    actual = Cheer.for_birthday("05/06")
+    expected = "Awesome! Your birthday is in 1 day! Happy Birthday in advance!"
+    assert_equal actual, expected
+  end
+
+  def test_birthday_yesterday
+    actual = Cheer.for_birthday("05/04")
+    expected = "Awesome! Your birthday is in 364 days! Happy Birthday in advance!"
+    assert_equal actual, expected
+  end
+
+  def test_birthday_in_near_future
+    actual = Cheer.for_birthday("06/02")
+    expected = "Awesome! Your birthday is in 28 days! Happy Birthday in advance!"
+    assert_equal actual, expected
+  end
+
+  def test_backwards_day
+    assert_raise(ArgumentError) do
+      Cheer.for_birthday("25/05")
+    end
+  end
+end
+
 end
